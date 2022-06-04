3444effaafc754de024d9b9e202526dc536edb5e
apicoding <apicodingprj@gmail.com>
2022-06-04 20:28:56 +0200

    Add project files.

---
+++ BloggingContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace EFGetStarted;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Valuation> Valuations { get; set; }
+
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+
+public class Stock
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    [MaxLength(8)]
+    public string Symbol { get; set; }
+
+    [OneToMany(CascadeOperations = CascadeOperation.All)]      // One to many relationship with Valuation
+    public List<Valuation> Valuations { get; set; }
+}
+
+public class Valuation
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [ForeignKey(typeof(Stock))]     // Specify the foreign key
+    public int StockId { get; set; }
+    public DateTime Time { get; set; }
+    public decimal Price { get; set; }
+
+    [ManyToOne]      // Many to one relationship with Stock
+    public Stock Stock { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using SQLite;
+using SQLiteNetExtensions.Extensions;
+
+namespace EFGetStarted;
+
+internal class Program
+{
+    private static void Main()
+    {
+        using var dbContext = new BloggingContext();
+
+   
+        // Note: This sample requires the database to be created before running.
+        Console.WriteLine($"Database path: {dbContext.DbPath}.");
+
+
+        //// Create
+        //Console.WriteLine("Inserting a new stock");
+        //dbContext.Add(new Stock { Id = 1, Symbol = "TEST", Valuations = new List<Valuation>()});
+        //dbContext.SaveChanges();
+
+
+
+        // Create
+        Console.WriteLine("Inserting a new blog");
+        dbContext.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+        dbContext.SaveChanges();
+
+        // Read
+        Console.WriteLine("Querying for a blog");
+        var blog = dbContext.Blogs
+            .OrderBy(b => b.BlogId)
+            .First();
+
+        // Update
+        Console.WriteLine("Updating the blog and adding a post");
+        blog.Url = "https://devblogs.microsoft.com/dotnet";
+        blog.Posts.Add(
+            new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+        dbContext.SaveChanges();
+
+        // Delete
+        Console.WriteLine("Delete the blog");
+        dbContext.Remove(blog);
+        dbContext.SaveChanges();
+
+        /*
+        var db = new SQLiteConnection(context.DbPath);
+        db.CreateTable<Stock>();
+        db.CreateTable<Valuation>();
+
+        var dollar = new Stock()
+        {
+            Symbol = "$"
+        };
+        db.Insert(dollar);   // Insert the object in the database
+
+        var valuation = new Valuation()
+        {
+            Price = 15,
+            Time = DateTime.Now,
+        };
+        db.Insert(valuation);   // Insert the object in the database
+
+        // Objects created, let's stablish the relationship
+        dollar.Valuations = new List<Valuation> { valuation };
+
+        db.UpdateWithChildren(dollar);   // Update the changes into the database
+        if (valuation.Stock == dollar)
+        {
+            Debug.WriteLine("Inverse relationship already set, yay!");
+        }
+
+        // Get the object and the relationships
+        var storedValuation = db.GetWithChildren<Valuation>(valuation.Id);
+        if (dollar.Symbol.Equals(storedValuation.Stock.Symbol))
+        {
+            Debug.WriteLine("Object and relationships loaded correctly!");
+        }
+        */
+    }
+}
